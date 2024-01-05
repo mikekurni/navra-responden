9bb016a277aa743b879326990a4e402db4aa672d
mikekurni <mike.kurni@gmail.com>
2024-01-06 05:53:00 +0700

    forcing to hide footer

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
+            footer {visibility: hidden !important;}
             header {visibility: hidden;}
             </style>
             """
