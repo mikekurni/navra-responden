c917cc198e2ad212c370e94b881670745c61a3b7
mikekurni <mike.kurni@gmail.com>
2024-01-08 13:01:22 +0700

    revamping app_v2.py. Adding back app.py to main-v2 branch

--- app_v2.py
+++ app_v2.py
@@ -63,6 +63,69 @@ action = st.selectbox(
     [
         "Tambah Data Baru",
         "Edit Data",
-        "Lihat Data",
     ],
 )
+
+if action == "Tambah Data Baru":
+    st.markdown("Tambah data responden baru pada formulir di bawah ini.")
+    with st.form(key="responden_form", clear_on_submit=True):
+        tanggal = st.date_input("Tanggal Data Diambil")
+        nama_koordinator = st.selectbox("Koordinator:red[*]", options=list_koordinator, index=None, placeholder="Pilih koordinator...")
+        nama_responden = st.text_input("Responden:red[*]", placeholder="Nama responden")
+        nik = str(st.number_input("Nomor Induk Kependudukan (NIK):red[*]", min_value=1, value=None, placeholder="NIK/No. KTP"))
+        dusun_jalan = st.text_input("Dusun/Jalan", placeholder="Alamat/dusun/jalan")
+        rt = st.number_input("RT:red[*]", min_value=1, value=None, step=1, placeholder="RT", help="Tidak perlu menambahkan angka 0 (nol) di depan.")
+        rw = st.number_input("RW:red[*]", min_value=1, value=None, step=1, placeholder="RW", help="Tidak perlu menambahkan angka 0 (nol) di depan.")
+        desa = st.selectbox("Desa :red[*]", options=list_desa, index=None, placeholder="Pilih desa...")
+        no_selular = st.number_input("Nomor HP", min_value=1, value=None, placeholder="No. HP Responden", help="Tidak perlu menambahkan angka 0 (nol) di depan")
+        keterangan = st.text_area("Keterangan Tambahan", placeholder="Tambahkan keterangan")
+
+        # Mark mandatory fields
+        st.markdown(":red[*wajib diisi]")
+
+        submit_button = st.form_submit_button(label="Tambah Data", type="primary")
+
+        if submit_button:
+            if not nama_koordinator or not nama_responden or not desa or not rt or not rw or not nik:
+                st.toast("Nama responden sudah terdata di dalam database.", icon='💽')
+            elif existing_data["NIK"].str.contains(nik).any():
+                st.toast("Nama responden sudah terdata di dalam database.", icon='💽')
+            else:
+                # Create a new row of respondent data
+                responden_data = pd.DataFrame(
+                    [
+                        {
+                            "TANGGAL": tanggal.strftime("%d-%m-%Y"),
+                            "NAMA_KOORDINATOR": nama_koordinator.upper(),
+                            "NAMA_RESPONDEN": nama_responden.upper(),
+                            "NIK": str(nik),
+                            "DUSUN_JALAN": dusun_jalan.upper(),
+                            "RT": rt,
+                            "RW": rw,
+                            "DESA": desa.upper(),
+                            "NO_SELULAR": str(no_selular),
+                            "KETERANGAN": keterangan.upper(),
+                        }
+                    ]
+                )
+                # Add the new responden to the existing data
+                update_df = pd.concat([existing_data, responden_data], ignore_index=True)
+
+                # Update Google Sheets with new respondent data
+                conn.update(worksheet="DATA", data=update_df)
+
+                st.toast("Data berhasil ditambahkan!", icon='🎉')
+
+elif action == "Update Existing Vendor":
+    st.markdown("Pilih data yang hendak diubah.")
+
+    responden_to_update = st.selectbox(
+        "Cari NIK responden yang hendak diubah", options=existing_data["NIK"].tolist()
+    )
+
+    responden_data = existing_data[existing_data["NIK"] == responden_to_update].iloc[0]
+
+    st.dataframe(responden_data)
+
+    # with st.form(key="update_responden_data"):
+    #     tanggal = st.date_input("Tanggal Data Diambil", value=pd.to_datetime(responden_data["TANGGAL"]))
